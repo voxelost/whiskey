b116c6bfaa7f5a613685db70a30ffe944f6208ed
antoni boryna <borynaantoni@gmail.com>
2022-03-13 15:17:35 +0100

    add default authorization commands

---
+++ commands/auth_commands.py
@@ -0,0 +1,13 @@
+import discord
+
+from class_definitions.client import Bot
+
+
+async def owner_only(message: discord.Message, bot: Bot, *args, **kwargs):
+    """allow only owner"""
+    if message.author.id != bot.owner_id:
+        raise Exception('unauthorized')
+
+
+async def anyone_anywhere(*args, **kwargs):
+    """allow anyone"""
